5d5c3f40047590c555046f19761099fbaf7b3246
Jeroen Boonstra <jeroen@provider.nl>
2016-11-22 13:35:37 +0100

    Built with template

--- spec/my_test_spec.rb
+++ spec/my_test_spec.rb
@@ -4,8 +4,12 @@ require 'dockerspec/infrataster'
 
 
 describe 'multibuild php test' do
-  describe docker_build('.') do
-    it { should have_expose '8000' }
+  docker_env = { 'SSH_PORT' => '22' }
+
+  describe docker_build(
+    template: 'Dockerfile1.erb', context: { 'phpversion' => ENV['PHP_VERSION']}
+  ) do
+    it { should have_expose '80', env: docker_env }
 
     describe docker_run(described_image) do
       describe server(described_container) do # Infrataster
