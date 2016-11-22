3c85f2fd1932223bd44e8420926b971991fa4fe0
Jeroen Boonstra <jeroen@provider.nl>
2016-11-22 16:23:49 +0100

    Added image tag

--- spec/my_test_spec.rb
+++ spec/my_test_spec.rb
@@ -5,9 +5,9 @@ require 'dockerspec/infrataster'
 
 describe 'multibuild php test' do
   docker_env = { 'SSH_PORT' => '22' }
-
+  image_tag = "dockerrepo.stack.provider.nl/test-php-" << ENV['PHP_VERSION']
   describe docker_build(
-  template: 'Dockerfile1.erb', context: { 'phpversion' => ENV['PHP_VERSION']}
+  template: 'Dockerfile1.erb', context: { 'phpversion' => ENV['PHP_VERSION']}, tag: image_tag
   ) do
     it { should have_expose '80', env: docker_env }
 
