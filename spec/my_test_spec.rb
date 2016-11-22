4143ff5db9f152ed54942d826d2c7e9eefd61f2d
Jeroen Boonstra <jeroen@provider.nl>
2016-11-22 13:59:33 +0100

    Removed webtest (not needed for this purpose) Fixed parameter build

--- spec/my_test_spec.rb
+++ spec/my_test_spec.rb
@@ -11,20 +11,7 @@ describe 'multibuild php test' do
   ) do
     it { should have_expose '80', env: docker_env }
 
-    describe docker_run(described_image) do
-      describe server(described_container) do # Infrataster
-
-        describe http('/') do
-          it 'responds content including "PHP"' do
-            expect(response.body).to include 'php'
-          end
-
-          it 'responds as "php" server' do
-            expect(response.headers['server']).to match(/php/i)
-          end
-        end
-
-      end
+    
     end
   end
 end
