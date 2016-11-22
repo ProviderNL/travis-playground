e7645201e4865732d2897efef65544dbf700cb81
Jeroen Boonstra <jeroen@provider.nl>
2016-11-22 14:09:42 +0100

    Fixed typo

--- spec/my_test_spec.rb
+++ spec/my_test_spec.rb
@@ -7,11 +7,10 @@ describe 'multibuild php test' do
   docker_env = { 'SSH_PORT' => '22' }
 
   describe docker_build(
-    template: 'Dockerfile1.erb', context: { 'phpversion' => ENV['PHP_VERSION']}
+  template: 'Dockerfile1.erb', context: { 'phpversion' => ENV['PHP_VERSION']}
   ) do
     it { should have_expose '80', env: docker_env }
 
-    
-    end
+
   end
 end
